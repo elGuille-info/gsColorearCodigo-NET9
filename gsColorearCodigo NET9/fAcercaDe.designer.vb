6e0edec37c6c1ceebc0a9cfa3bb438d003b7df36
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2026-06-15 22:56:03 +0200

    Initial commit: Estructura del proyecto en .NET 9.0 con librería VBCompat

---
+++ gsColorearCodigo NET9/fAcercaDe.designer.vb
@@ -0,0 +1,233 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fAcercaDe
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(fAcercaDe))
+        TableLayoutPanel1 = New TableLayoutPanel()
+        labelWeb = New Label()
+        labelDescripcion = New Label()
+        labelAutor = New Label()
+        labelTitulo = New Label()
+        btnAceptar = New Button()
+        linkBug = New LinkLabel()
+        linkPrograma = New LinkLabel()
+        ToolTip1 = New ToolTip(components)
+        linkURL = New LinkLabel()
+        timerWeb = New Timer(components)
+        TableLayoutPanel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        TableLayoutPanel1.BackColor = Color.Transparent
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 474F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 140F))
+        TableLayoutPanel1.Controls.Add(labelWeb, 0, 3)
+        TableLayoutPanel1.Controls.Add(labelDescripcion, 0, 2)
+        TableLayoutPanel1.Controls.Add(labelAutor, 0, 5)
+        TableLayoutPanel1.Controls.Add(labelTitulo, 0, 0)
+        TableLayoutPanel1.Controls.Add(btnAceptar, 1, 8)
+        TableLayoutPanel1.Controls.Add(linkBug, 0, 7)
+        TableLayoutPanel1.Controls.Add(linkPrograma, 0, 8)
+        TableLayoutPanel1.Location = New Point(386, 15)
+        TableLayoutPanel1.Margin = New Padding(6, 5, 6, 5)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 9
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 122F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 8F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 303F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 92F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 8F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 52F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 8F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 50F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 227F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 38F))
+        TableLayoutPanel1.Size = New Size(614, 700)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' labelWeb
+        ' 
+        TableLayoutPanel1.SetColumnSpan(labelWeb, 2)
+        labelWeb.Dock = DockStyle.Fill
+        labelWeb.Font = New Font("Tahoma", 9F)
+        labelWeb.ForeColor = Color.White
+        labelWeb.Location = New Point(6, 433)
+        labelWeb.Margin = New Padding(6, 0, 6, 0)
+        labelWeb.Name = "labelWeb"
+        labelWeb.Size = New Size(602, 92)
+        labelWeb.TabIndex = 2
+        labelWeb.Text = "Info actualización" & vbCrLf & "Segunda línea" & vbCrLf & "Tercera"
+        ' 
+        ' labelDescripcion
+        ' 
+        TableLayoutPanel1.SetColumnSpan(labelDescripcion, 2)
+        labelDescripcion.Dock = DockStyle.Fill
+        labelDescripcion.Font = New Font("Tahoma", 9F, FontStyle.Bold)
+        labelDescripcion.ForeColor = Color.White
+        labelDescripcion.Location = New Point(6, 130)
+        labelDescripcion.Margin = New Padding(6, 0, 6, 0)
+        labelDescripcion.Name = "labelDescripcion"
+        labelDescripcion.Padding = New Padding(7, 8, 13, 8)
+        labelDescripcion.Size = New Size(602, 303)
+        labelDescripcion.TabIndex = 1
+        labelDescripcion.Text = "gsColorearCodigo v1.0.0.0"
+        ' 
+        ' labelAutor
+        ' 
+        TableLayoutPanel1.SetColumnSpan(labelAutor, 2)
+        labelAutor.Dock = DockStyle.Fill
+        labelAutor.Font = New Font("Tahoma", 9.75F, FontStyle.Bold)
+        labelAutor.ForeColor = Color.White
+        labelAutor.Location = New Point(6, 533)
+        labelAutor.Margin = New Padding(6, 0, 6, 0)
+        labelAutor.Name = "labelAutor"
+        labelAutor.Size = New Size(602, 52)
+        labelAutor.TabIndex = 3
+        labelAutor.Text = "©Guillermo 'guille' Som, 2005-2007"
+        labelAutor.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' labelTitulo
+        ' 
+        TableLayoutPanel1.SetColumnSpan(labelTitulo, 2)
+        labelTitulo.Dock = DockStyle.Fill
+        labelTitulo.Font = New Font("Tahoma", 24F, FontStyle.Bold)
+        labelTitulo.ForeColor = Color.White
+        labelTitulo.Location = New Point(6, 0)
+        labelTitulo.Margin = New Padding(6, 0, 6, 0)
+        labelTitulo.Name = "labelTitulo"
+        labelTitulo.Size = New Size(602, 122)
+        labelTitulo.TabIndex = 0
+        labelTitulo.Text = "gsColorearCodigo"
+        ' 
+        ' btnAceptar
+        ' 
+        btnAceptar.BackColor = Color.DarkBlue
+        btnAceptar.FlatAppearance.BorderColor = Color.White
+        btnAceptar.FlatAppearance.MouseDownBackColor = Color.RoyalBlue
+        btnAceptar.FlatAppearance.MouseOverBackColor = Color.SlateBlue
+        btnAceptar.FlatStyle = FlatStyle.Flat
+        btnAceptar.ForeColor = Color.White
+        btnAceptar.Location = New Point(480, 648)
+        btnAceptar.Margin = New Padding(6, 5, 6, 5)
+        btnAceptar.Name = "btnAceptar"
+        btnAceptar.Size = New Size(126, 45)
+        btnAceptar.TabIndex = 5
+        btnAceptar.Text = "Aceptar"
+        btnAceptar.UseVisualStyleBackColor = False
+        ' 
+        ' linkBug
+        ' 
+        linkBug.ActiveLinkColor = Color.White
+        TableLayoutPanel1.SetColumnSpan(linkBug, 2)
+        linkBug.Dock = DockStyle.Fill
+        linkBug.Font = New Font("Tahoma", 9.75F, FontStyle.Bold)
+        linkBug.LinkColor = Color.White
+        linkBug.Location = New Point(6, 593)
+        linkBug.Margin = New Padding(6, 0, 6, 0)
+        linkBug.Name = "linkBug"
+        linkBug.Size = New Size(602, 50)
+        linkBug.TabIndex = 4
+        linkBug.TabStop = True
+        linkBug.Text = "Reportar un bug o una mejora"
+        ToolTip1.SetToolTip(linkBug, " Pulsa en este link para reportar un bug o alguna mejora (te llevará al repositorio de github)")
+        linkBug.VisitedLinkColor = Color.White
+        ' 
+        ' linkPrograma
+        ' 
+        linkPrograma.ActiveLinkColor = Color.White
+        linkPrograma.AutoSize = True
+        linkPrograma.Font = New Font("Tahoma", 9.75F, FontStyle.Bold)
+        linkPrograma.LinkColor = Color.White
+        linkPrograma.Location = New Point(6, 643)
+        linkPrograma.Margin = New Padding(6, 0, 6, 0)
+        linkPrograma.Name = "linkPrograma"
+        linkPrograma.Size = New Size(284, 24)
+        linkPrograma.TabIndex = 6
+        linkPrograma.TabStop = True
+        linkPrograma.Text = "Ir a la página de la utilidad"
+        ToolTip1.SetToolTip(linkPrograma, " Ir a https://www.elguillemola.com/gscolorearcodigo-net9/")
+        linkPrograma.VisitedLinkColor = Color.White
+        ' 
+        ' linkURL
+        ' 
+        linkURL.ActiveLinkColor = Color.White
+        linkURL.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        linkURL.BackColor = Color.Transparent
+        linkURL.DisabledLinkColor = Color.White
+        linkURL.Font = New Font("Tahoma", 9.75F, FontStyle.Bold)
+        linkURL.LinkColor = Color.White
+        linkURL.Location = New Point(13, 663)
+        linkURL.Margin = New Padding(6, 0, 6, 0)
+        linkURL.Name = "linkURL"
+        linkURL.Padding = New Padding(7, 0, 0, 0)
+        linkURL.Size = New Size(361, 45)
+        linkURL.TabIndex = 1
+        linkURL.TabStop = True
+        linkURL.Text = "https://elguillemola.com"
+        ToolTip1.SetToolTip(linkURL, " Ir a mi blog")
+        linkURL.VisitedLinkColor = Color.White
+        ' 
+        ' timerWeb
+        ' 
+        ' 
+        ' fAcercaDe
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1013, 730)
+        Controls.Add(linkURL)
+        Controls.Add(TableLayoutPanel1)
+        ForeColor = SystemColors.WindowText
+        FormBorderStyle = FormBorderStyle.None
+        Margin = New Padding(6, 5, 6, 5)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "fAcercaDe"
+        Opacity = 0.95R
+        Padding = New Padding(13, 15, 13, 15)
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Acerca de..."
+        TableLayoutPanel1.ResumeLayout(False)
+        TableLayoutPanel1.PerformLayout()
+        ResumeLayout(False)
+
+    End Sub
+    Private WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Private WithEvents btnAceptar As System.Windows.Forms.Button
+    Private WithEvents labelAutor As System.Windows.Forms.Label
+    Private WithEvents labelDescripcion As System.Windows.Forms.Label
+    Private WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Private WithEvents linkBug As System.Windows.Forms.LinkLabel
+    Private WithEvents linkURL As System.Windows.Forms.LinkLabel
+    Private WithEvents timerWeb As System.Windows.Forms.Timer
+    Private WithEvents labelWeb As System.Windows.Forms.Label
+    Private WithEvents linkPrograma As System.Windows.Forms.LinkLabel
+    Private WithEvents labelTitulo As Label
+End Class
---
+++ gsColorearCodigo NET9/fAcercaDe.vb
@@ -0,0 +1,238 @@
+'------------------------------------------------------------------------------
+' Formulario Acerca de "el Guille"                                  (12/Jul/06)
+' Adaptado del AcercaDe del visor de imágenes.
+'
+'
+' ©Guillermo 'guille' Som, 2006, 2020, 2026
+'------------------------------------------------------------------------------
+Option Explicit On
+Option Strict On
+
+Imports Microsoft.VisualBasic
+Imports vb = Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports System.Drawing
+
+'Imports clickOnce = System.Deployment
+Imports System.Text.RegularExpressions
+
+Public Class fAcercaDe
+
+    ' Datos a configurar en cada uso de este formulario:
+
+    ' Nueva URL de la utilidad en el blog                       (15/jun/26 21.27)
+
+    Const laURL As String = "https://www.elguillemola.com/gscolorearcodigo-net9/"
+    ' Para mover la ventana
+    Private ratonPulsado As Boolean
+    Private pX, pY As Integer
+    Private bugInfo As String
+    'Private fvi As System.Diagnostics.FileVersionInfo
+    Private ficVers As String = ""
+    '
+    Private Sub fAcercaDe_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim ensamblado As System.Reflection.Assembly = System.Reflection.Assembly.GetExecutingAssembly
+        'fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(ensamblado.Location)
+
+
+        Dim copyrAttr = ensamblado.GetCustomAttributes(GetType(System.Reflection.AssemblyCopyrightAttribute), False)
+        Dim copyr = If(copyrAttr.Length > 0, TryCast(copyrAttr(0), System.Reflection.AssemblyCopyrightAttribute).Copyright,
+                                            "Copyright © Guillermo Som (Guille), 2020, 2026")
+        ' Información de Copyright
+        labelAutor.Text = copyr
+
+        Dim versionAttr = ensamblado.GetCustomAttributes(GetType(System.Reflection.AssemblyVersionAttribute), False)
+        Dim vers = If(versionAttr.Length > 0, TryCast(versionAttr(0), System.Reflection.AssemblyVersionAttribute).Version,
+                                                "2.0.0.0")
+        Dim fileVerAttr = ensamblado.GetCustomAttributes(GetType(System.Reflection.AssemblyFileVersionAttribute), False)
+        Dim versF = If(fileVerAttr.Length > 0, TryCast(fileVerAttr(0), System.Reflection.AssemblyFileVersionAttribute).Version,
+                                                "2.0.0.0")
+        ficVers = vers
+
+        Dim prodAttr = ensamblado.GetCustomAttributes(GetType(System.Reflection.AssemblyProductAttribute), False)
+        Dim producto = If(prodAttr.Length > 0, TryCast(prodAttr(0), System.Reflection.AssemblyProductAttribute).Product,
+                                                "gsColorearCodigo NET9")
+
+        ' La descripción, tomar solo el final                   (11/Sep/20)
+        Dim descAttr = ensamblado.GetCustomAttributes(GetType(System.Reflection.AssemblyDescriptionAttribute), False)
+        Dim desc = If(descAttr.Length > 0, TryCast(descAttr(0), System.Reflection.AssemblyDescriptionAttribute).Description,
+                                                "(para .NET 9.0 revisión del 15/Jun/2026)")
+        desc = desc.Substring(desc.IndexOf("(para .NET"))
+
+        labelTitulo.Text = producto
+        bugInfo = $"{labelTitulo.Text} v {versF}"
+
+        Dim descColorear = gsColorearNET.Colorear.Version(True)
+        Dim i = descColorear.IndexOf("(para .NET")
+        descColorear = descColorear.Substring(0, i) & vbCrLf & descColorear.Substring(i)
+
+        labelDescripcion.Text = labelTitulo.Text &
+                        $" v{vers} ({versF})" & vbCrLf & vbCrLf &
+                        desc & vbCrLf & vbCrLf &
+                        descColorear
+
+
+        Me.labelWeb.Text = "Comprobando si hay actualizaciones..."
+        timerWeb.Enabled = True
+    End Sub
+
+    Private Sub pMouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) _
+                    Handles Me.MouseDown, TableLayoutPanel1.MouseDown,
+                            labelTitulo.MouseDown, labelDescripcion.MouseDown, labelAutor.MouseDown
+        ' Mover el formulario mientras se mantenga el ratón pulsado
+        ratonPulsado = True
+        pX = e.X
+        pY = e.Y
+    End Sub
+    Private Sub pMouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) _
+                    Handles Me.MouseMove, TableLayoutPanel1.MouseMove,
+                            labelTitulo.MouseMove, labelDescripcion.MouseMove, labelAutor.MouseMove
+        If ratonPulsado Then
+            Me.Left += e.X - pX
+            Me.Top += e.Y - pY
+        End If
+    End Sub
+    Private Sub pMouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) _
+                    Handles Me.MouseUp, TableLayoutPanel1.MouseUp,
+                            labelTitulo.MouseUp, labelDescripcion.MouseUp, labelAutor.MouseUp
+        ratonPulsado = False
+    End Sub
+
+    Private Sub LinkURL_LinkClicked(sender As System.Object,
+                                    e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles linkURL.LinkClicked
+        Try
+            'System.Diagnostics.Process.Start(linkURL.Text)
+            Dim p As New Process
+            p.StartInfo.FileName = linkURL.Text
+            p.StartInfo.UseShellExecute = True
+            p.Start()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error al abrir la página de la utilidad")
+        End Try
+
+        'End If
+    End Sub
+
+    Private Sub btnAceptar_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub linkBug_LinkClicked(ByVal sender As Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles linkBug.LinkClicked
+        ' Enviar un bug o mejora                                    (13/Jul/06)
+        'If My.Computer.Network.IsAvailable Then
+        'Dim sb As New System.Text.StringBuilder
+        Dim sb = "https://github.com/elGuille-info/gsColorearCodigo-NET9"
+        Try
+            'System.Diagnostics.Process.Start(sb.ToString)
+            Dim p As New Process
+            p.StartInfo.FileName = sb
+            p.StartInfo.UseShellExecute = True
+            p.Start()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error al abrir la página de reportar bug o mejora")
+        End Try
+
+        'End If
+    End Sub
+    '
+    Private Function versionWeb() As String
+        Try
+            Dim request As System.Net.WebRequest = System.Net.WebRequest.Create(laURL)
+            Dim response As System.Net.WebResponse
+            Dim reader As System.IO.StreamReader
+            ' Obtener la respuesta.
+            response = request.GetResponse()
+            ' Abrir el stream de la respuesta recibida.
+            reader = New System.IO.StreamReader(response.GetResponseStream())
+            ' Leer el contenido.
+            Dim s As String = reader.ReadToEnd()
+            ' Cerrar los streams abiertos.
+            reader.Close()
+            response.Close()
+            '
+            ' Comprobar el valor de <meta name="version" 
+            ' Usar esta expresión regular: <meta name="version" content="(\d.\d.\d.\d)" />
+            ' En Groups(1) estará la versión
+            Dim r As New Regex("<meta name=""version"" content=""(\d{1,}.\d{1,}.\d{1,}.\d{1,})"" />")
+            For Each m As Match In r.Matches(s)
+                If m.Groups.Count > 1 Then
+                    Return m.Groups(1).Value
+                End If
+            Next
+        Catch ex As Exception
+            Return ""
+        End Try
+        Return ""
+    End Function
+    '
+    Private Sub timerWeb_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles timerWeb.Tick
+        timerWeb.Enabled = False
+
+        Dim sb As New System.Text.StringBuilder
+        'If clickOnce.Application.ApplicationDeployment.IsNetworkDeployed Then
+        '    sb.Append("Aplicación instalada con ClickOnce")
+        '    sb.AppendLine()
+        '    Dim inf As clickOnce.Application.UpdateCheckInfo
+        '    inf = My.Application.Deployment.CheckForDetailedUpdate
+        '    If inf.UpdateAvailable Then
+        '        sb.AppendFormat("Hay una nueva versión disponible: v{0}", inf.AvailableVersion.ToString)
+        '    Else
+        '        sb.Append("Esta es la versión más reciente, no hay nuevas actualizaciones.")
+        '    End If
+        'Else
+        sb.Append("Aplicación instalada directamente (sin usar ClickOnce)")
+        sb.AppendLine()
+        Dim vWeb As String = "2.0.0.0" ' versionWeb()
+        '' Por si la versión está vacía                          (17/Abr/07)
+        'If String.IsNullOrEmpty(vWeb) Then
+        '    vWeb = "0.0.0.0"
+        'End If
+
+        ' Solo funcionará bien con valores de 1 cifra           (14/Feb/07)
+        ' ya que 1.0.3.11 será menor que 1.0.3.9 aunque no sea así...
+        ' Convertirlo en cadena de números de dos cifras
+        Dim aWeb() As String = vWeb.Split("."c)
+        Dim aFic() As String = ficVers.Split("."c)
+        Dim wMayor As Boolean = False
+        vWeb = ""
+        Dim sFic As String = ""
+        ' Para que se muestre bien, añadir puntos               (17/Abr/07)
+        For i As Integer = 0 To aWeb.Length - 1
+            vWeb &= CInt(aWeb(i)).ToString("00") & "."
+            'sFic &= CInt(aFic(i)).ToString("00")
+        Next
+        For i As Integer = 0 To aFic.Length - 1
+            sFic &= CInt(aFic(i)).ToString("00") & "."
+        Next
+        If vWeb > sFic Then ' fvi.FileVersion Then
+            sb.AppendFormat("Hay una nueva versión disponible en mi sitio: v{0}", vWeb)
+            'sb.AppendFormat("La última versión en la Web es: v{0}, la actual es: v{1}", vWeb, fvi.FileVersion)
+        Else
+            sb.Append("Esta es la versión más reciente, no hay nuevas actualizaciones.")
+        End If
+        'End If
+
+        Me.labelWeb.Text = sb.ToString
+    End Sub
+
+    Private Sub linkPrograma_LinkClicked(ByVal sender As System.Object,
+                                         ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles linkPrograma.LinkClicked
+        ' Ir a la página de la utilidad                             (14/Feb/07)
+        'If My.Computer.Network.IsAvailable Then
+        '    System.Diagnostics.Process.Start(laURL)
+        'End If
+        Try
+            'System.Diagnostics.Process.Start(laURL)
+            Dim p As New Process
+            p.StartInfo.FileName = laURL
+            p.StartInfo.UseShellExecute = True
+            p.Start()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error al abrir la página de utilidad")
+        End Try
+    End Sub
+End Class
